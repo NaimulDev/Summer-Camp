767b427a247c5f60e1ad34d5ff9ff3181a820073
Nimul99 <htnimul99@gmail.com>
2023-06-12 21:21:39 +0600

    Admin Dashboard Implement

--- src/Pages/Home/DashboardPages/AdminPanel/ManageUsers/ManageUsers.jsx
+++ src/Pages/Home/DashboardPages/AdminPanel/ManageUsers/ManageUsers.jsx
@@ -1,91 +1,153 @@
-import React from "react";
+// import { useQuery } from "@tanstack/react-query";
+// import DashSectionTitle from "../../../component/DashboardSectionTitle";
 import { Helmet } from "react-helmet-async";
-import { FaTrashAlt, FaUserShield } from "react-icons/fa";
-import useAxiosSecure from "../../../../../hooks/useAxiosSecure";
 import Swal from "sweetalert2";
+import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../../../hooks/useAxiosSecure";
 
-const ManageUsers = () => {
+const ManageAllUser = () => {
+  const [isButtonDisabled, setButtonDisabled] = useState(false);
+
   const [axiosSecure] = useAxiosSecure();
   const { data: users = [], refetch } = useQuery(["users"], async () => {
     const res = await axiosSecure.get("/users");
     return res.data;
   });
-
-  const handleMakeAdmin = (user) => {
-    fetch(`http://localhost:5000/users/admin/${user._id}`, {
-      method: "PATCH",
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        if (data.modifiedCount) {
-          refetch();
-          Swal.fire({
-            position: "top-end",
-            icon: "success",
-            title: `${user.name} is an Admin Now!`,
-            showConfirmButton: false,
-            timer: 1500,
+  const handlerMakeAdmin = (user) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "Your web site New Admin selected",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#173931",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes.!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`${import.meta.env.VITE_API_URL}/users/admin/${user._id}`, {
+          method: "PATCH",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.modifiedCount) {
+              refetch();
+              setButtonDisabled(true);
+              Swal.fire("Admin!", `${user.name} is an Admin Now!!`, "success");
+            }
           });
-        }
-      });
+      }
+    });
   };
-
-  const handleDelete = (user) => {};
+  const handlerMakeInstructor = (user) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "Your web site New Admin selected",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#173931",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes.!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`${import.meta.env.VITE_API_URL}/users/instractor/${user._id}`, {
+          method: "PATCH",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.modifiedCount) {
+              refetch();
+              setButtonDisabled(true);
+              Swal.fire(
+                "Instructor!",
+                `${user.name} is an Instructor !`,
+                "success"
+              );
+            }
+          });
+      }
+    });
+  };
   return (
-    <div className="w-full">
+    <div>
       <Helmet>
-        <title>Pallikoodam | Manage Users</title>
+        <title>Pallikoodam || Manage Users</title>
       </Helmet>
-      <h3 className="text-3xl font-semibold my-4">
-        Total Users: {users.length}
-      </h3>
-      <div className="overflow-x-auto">
-        <table className="table table-zebra w-full">
+      {/* <DashSectionTitle
+                name='Admin'
+                title='Manage All Users'
+                subTitle='Site all Users and Position!'
+            /> */}
+      <div
+        data-aos="fade-down"
+        data-aos-easing="linear"
+        data-aos-duration="1500"
+        className="overflow-x-auto"
+      >
+        <table className="table">
           {/* head */}
           <thead>
-            <tr>
-              <th>#</th>
+            <tr className=" text-lg">
+              <th>Image</th>
               <th>Name</th>
               <th>Email</th>
               <th>Role</th>
-              <th>Action</th>
+              <th className="pl-14">Action</th>
             </tr>
           </thead>
-          <tbody>
-            {users.map((user, index) => (
-              <tr key={user._id}>
-                <th>{index + 1}</th>
-                <td>{user.name}</td>
-                <td>{user.email}</td>
+          {users.map((user) => (
+            <tbody key={user._id}>
+              <tr className="">
                 <td>
-                  {user.role === "admin" ? (
-                    "admin"
+                  <div className="mask mask-squircle w-12 h-12">
+                    <img
+                      src={user?.image}
+                      alt="Avatar Tailwind CSS Component"
+                    />
+                  </div>
+                </td>
+                <td>{user?.name}</td>
+                <td>{user?.email}</td>
+                <td>
+                  {user?.role === "admin" ? (
+                    <span className="text-lg font-semibold text-red-800">
+                      Admin
+                    </span>
+                  ) : user?.role === "instractor" ? (
+                    <span className="text-lg font-semibold text-yellow-600">
+                      Instructor
+                    </span>
                   ) : (
-                    <button
-                      onClick={() => handleMakeAdmin(user)}
-                      className="btn btn-ghost bg-orange-600  text-white"
-                    >
-                      <FaUserShield></FaUserShield>
-                    </button>
+                    <span className="text-base font-medium tracking-wider text-gray-900">
+                      User
+                    </span>
                   )}
                 </td>
-                <td>
+                <td className="flex items-center gap-1">
                   <button
-                    onClick={() => handleDelete(user)}
-                    className="btn btn-ghost bg-red-600  text-white"
+                    onClick={() => handlerMakeAdmin(user)}
+                    disabled={isButtonDisabled}
+                    className="bg-main_color bg-orange-500 text-white w-full py-2 rounded-lg shadow-xl"
                   >
-                    <FaTrashAlt></FaTrashAlt>
+                    Make Admin
+                  </button>
+                  <button
+                    onClick={() => handlerMakeInstructor(user)}
+                    disabled={isButtonDisabled}
+                    className="bg-main_color bg-orange-500 text-white w-full py-2 rounded-lg shadow-xl"
+                  >
+                    Make Instructor
                   </button>
                 </td>
               </tr>
-            ))}
-          </tbody>
+            </tbody>
+          ))}
         </table>
       </div>
     </div>
   );
 };
 
-export default ManageUsers;
+export default ManageAllUser;
